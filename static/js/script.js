65f50a4df1f075fe05abc474ded7c640194db50e
safety-panda <earmiffs+git@googlemail.com>
2022-11-09 14:26:55 +0000

    added buttons and js file

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+const greyBtn = document.getElementById("greyBtn");
+const pinkBtn = document.getElementById("pinkBtn");
+
+greyBtn.addEventListener("click", function onClick(event) {
+  document.body.style.backgroundColor = "#C6D8D5";
+});
+
+pinkBtn.addEventListener("click", function onClick(event) {
+  document.body.style.backgroundColor = "#F9C7E4";
+});
