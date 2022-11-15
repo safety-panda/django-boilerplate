ec2a41a921a6e0ab92458866de8f54c1b22e9277
safety-panda <earmiffs+git@googlemail.com>
2022-11-15 17:08:01 +0000

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ module.exports = {
     target: "es2015",
     rollupOptions: {
       input: {
-        main: resolve("./static/src/js/main.js"),
+        script: resolve("./static/js/script.js"),
       },
       output: {
         chunkFileNames: undefined,
